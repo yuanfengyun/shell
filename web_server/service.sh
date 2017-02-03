3c5616b864ee89d05aa2278fdad0539c9dc357f1
yuanfengyun <yfy@ejoy.com>
2017-02-03 10:40:54 +0800

    加入crontab用法示例

--- web_server/service.sh
+++ web_server/service.sh
@@ -3,10 +3,9 @@
 base=/var/www
 
 read request
+cat $request >> a.txt
 path=`echo $request | cut -d' ' -f2`
 file_path="${base}${path}"
-echo $file_path > a.txt
-
 len=`wc -c $file_path | cut -d' ' -f1`
 echo -ne "HTTP/1.1 200 OK\r\n"
 echo -ne "Content-Length:${len}\r\n"
